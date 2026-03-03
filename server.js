b120c62475f7c788ffda307d34fb6255f3bd73d1
tulipbot666-eng <tulipbot666@gmail.com>
2026-03-03 14:34:59 -0300

    Update server.js

--- server.js
+++ server.js
@@ -90,7 +90,8 @@ initDB().catch(console.error);
 // ─────────────────────────────────────────
 // SESSION + PASSPORT
 // ─────────────────────────────────────────
-app.use(express.json());
+app.use(express.json({ limit: '15mb' }));
+app.use(express.urlencoded({ extended: true, limit: '15mb' }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.set('trust proxy', 1); // necessário para cookies seguros atrás do proxy do Render
