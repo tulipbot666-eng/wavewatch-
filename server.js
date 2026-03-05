d442ff31c00b3e09066db2dab05e98623061fbef
tulipbot666-eng <tulipbot666@gmail.com>
2026-03-04 23:28:55 -0300

    Update server.js

--- server.js
+++ server.js
@@ -149,7 +149,7 @@ app.use(express.urlencoded({ extended: true, limit: '15mb' }));
 app.use((req, res, next) => {
   res.setHeader('Content-Security-Policy',
     "default-src 'self'; " +
-    "script-src 'self' 'unsafe-inline' https://cdn.jsdelivr.net https://cdnjs.cloudflare.com https://www.youtube.com https://s.ytimg.com https://apis.google.com; " +
+    "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://cdn.jsdelivr.net https://cdnjs.cloudflare.com https://www.youtube.com https://s.ytimg.com https://apis.google.com; " +
     "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com https://cdnjs.cloudflare.com https://cdn.jsdelivr.net; " +
     "font-src 'self' data: https://fonts.gstatic.com https://cdnjs.cloudflare.com https://cdn.jsdelivr.net; " +
     "frame-src https://www.youtube.com https://player.vimeo.com https://player.twitch.tv https://www.twitch.tv; " +
